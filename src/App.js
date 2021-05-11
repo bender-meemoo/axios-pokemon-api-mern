8fecd957765eca92e3cad05be4fd7a6d8fc19967
Eric H. Smith <esmith167@gmail.com>
2021-05-11 17:45:34 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState,useEffect } from 'react' ;
+import axios from 'axios';
 
 function App() {
+
+const [poke, setPoke] = useState([]);
+
+const onClickHandler = (event) => {
+  axios.get ("https://pokeapi.co/api/v2/pokemon?offset=807&limit=311")
+    .then(response => {
+      console.log(response);
+      setPoke(response.data.results);
+    })
+  // .then( response => response.json())
+  // .then(response => console.log(response.results))
+  // console.log("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+  // .then (response => {
+    // console.log("^^^^^TEST^^^^^^")
+    // setPoke(response.results)
+    // console.log(poke)
+  // })
+  // .catch(err => console.log(err))
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <button type="submit" className="btn btn-secondary" onClick = {onClickHandler}>Catch them all?</button>
+      <ul>
+        {
+          poke.map( (item, i) => <li key={ i }> {item.name} </li>)
+        }
+
+      </ul>
+      
     </div>
   );
 }
 
 export default App;
+
